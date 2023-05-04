508535e720b526ffecca747223eaae3f9e998398
wellpinho <wellpinho@outlook.com>
2023-05-04 20:49:58 -0300

    search domain component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,6 +77,103 @@ export default function Home() {
             </div>
         </div>
       </section>
+
+      {/* shorten section */}
+      <section id="shorten" className="relative bg-gray-100">
+        {/* shorten container */}
+        <div className="max-w-4xl mx-auto p-6 space-y-6">
+          {/* form */}
+          <form id="link-form" className="
+            relative 
+            flex 
+            flex-col 
+            w-full 
+            p-10 
+            -mt-20 
+            space-y-4 
+            bg-darkViolet 
+            rounded-lg 
+            md:flex-row 
+            md:space-y-0 
+            md:space-x-3
+          ">
+            <input type="text" 
+              className="
+                flex-1 
+                p-3 
+                border-2 
+                rounded-lg 
+                placehold-yellow-500 
+                focus:outline-none
+              " 
+              placeholder="Shorten a link here"
+              id="link-input"
+            />
+
+            <button className="
+              px-10 
+              py-3 
+              text-white 
+              bg-cyan 
+              rounded-lg 
+              hover:bg-cyanLight 
+              hover:text-veryDarkBlue 
+              focus:outline-none 
+              md:py-2
+            ">
+              Shorten It!
+            </button>
+          </form>
+
+          {/* link 1 */}
+          <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+            <p className="font-bold text-center text-veryDarkViolet md:text-left">
+              https://www.linkedin.com/in/wellpinho/
+            </p>
+
+            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+              <div className="font-bold text-cyan">
+                https://rel.ink/k4IKyk
+              </div>
+              <button className="p-2 px-8 text-white bg-cyan rounded-md hover:opacity-70 focus:outline-none">
+                Copy
+              </button>
+            </div>
+          </div>
+
+          {/* link 2 */}
+          <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+            <p className="font-bold text-center text-veryDarkViolet md:text-left">
+              https://github.com/wellpinho
+            </p>
+
+            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+              <div className="font-bold text-cyan">
+                https://rel.ink/HJ4fscc
+              </div>
+              <button className="p-2 px-8 text-white bg-darkViolet rounded-md hover:opacity-70 focus:outline-none">
+                Copy
+              </button>
+            </div>
+          </div>
+
+          {/* link 3 */}
+          <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+            <p className="font-bold text-center text-veryDarkViolet md:text-left">
+              https://wellpinho.com
+            </p>
+
+            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+              <div className="font-bold text-cyan">
+                https://rel.ink/k4ttF4
+              </div>
+              <button className="p-2 px-8 text-white bg-cyan rounded-md hover:opacity-70 focus:outline-none">
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
