5d64a08569f3d517b2e5210a40ad58604be5e77d
wellpinho <wellpinho@outlook.com>
2023-05-06 18:13:54 -0300

    security

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -216,6 +216,46 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* stats section */}
+      <section id="stats" className="py-24 bg-gray-100">
+        <div className="container mx-auto px-3">
+          <h2 className="text-4xl mb-6 font-bold text-center">
+            Advanced Statistics
+          </h2>
+          <p className="max-w-xs mx-auto text-center text-gray-400 md:max-w-md">
+            Track how your links are performing across the web with our advanced statistics dashboard.
+          </p>
+        </div>
+      </section>
+
+      {/* feature box section */}
+      <section id="features" className="pb-32 bg-gray-100">
+        <div className="relatice container flex flex-col imtes-start px-6 mx-auto md:flex-row md:space-x-7">
+          {/* horizontal line */}
+          <div className="hidden absolute top-24 w-10/12 left-16 h-3 bg-cyan md:block"></div>
+          {/* vertical line */}
+          <div className="absolute w-2 h-full -ml-1 left-1/2 bg-cyan md:hidden"></div>
+
+          {/* box 1 */}
+          <div className="relative flex flex-col p-6 space-y-6 md:w-1/3 bg-white rounded-lg">
+            {/* image positionng */}
+            <div className="absolute -ml-10 -top-10 left-1/2 md:left-16">
+              {/* image container for background & center */}
+              <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-veryDarkBlue">
+                <img src="/images/icon-brand-recognition.svg" alt="" />
+              </div>
+              <h5 className="pt-6 text-xl font-bold text-bold text-center captalize md:flex-left">
+                Brand Recognition
+              </h5>
+              <p className="text-center text-gray-400 md:text-left">
+                Boost your brand recognition with each click, Generic links don&apos;t mean a thing.
+                Brand links help instil confidence in your content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
